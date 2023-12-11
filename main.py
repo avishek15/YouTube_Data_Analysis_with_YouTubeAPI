26809e5218f4a21f88da54011be7c7c023f0fc46
Avishek Majumder <avishekalex15@gmail.com>
2023-12-11 18:30:52 +0530

    Update main.py
    Downloaded Sample Data

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ from config import channel_list
 from channel_functions import get_channel_ids, get_channel_stats, get_all_videos, get_video_details
 from translator import translate
 
-
 _ = load_dotenv()
+
+from together_llm import ask_llm_to_extract_keywords
+
+
 
 def flatten(lol):
 	l = []
@@ -23,34 +26,50 @@ def flatten(lol):
 
 def main():
 
-	print("Getting Channel IDs")
+	# print("Getting Channel IDs")
 
-	youtube = build('youtube', 'v3', developerKey=os.environ['api_key'])
-	channel_id_dict = get_channel_ids(youtube, channel_list)
+	# youtube = build('youtube', 'v3', developerKey=os.environ['api_key'])
+	# channel_id_dict = get_channel_ids(youtube, channel_list)
 
-	print("Channel stats to Dataframe")
+	# print("Channel stats to Dataframe")
 
-	channel_stats_df = pd.DataFrame(get_channel_stats(youtube, list(channel_id_dict.values())))
+	# channel_stats_df = pd.DataFrame(get_channel_stats(youtube, list(channel_id_dict.values())))
 
-	print("Getting all videos in channels")
+	# print("Getting all videos in channels")
 
-	all_video_list = get_all_videos(youtube, list(channel_stats_df['Channel_playlist']))
+	# all_video_list = get_all_videos(youtube, list(channel_stats_df['Channel_playlist']))
 
-	print("Getting video details")
+	# # Eliminate the videos already processed in a previous timestamp
 
-	main_database = get_video_details(youtube, flatten(all_video_list))
+	# print("Getting video details")
 
-	all_descriptions = main_database['descriptions']
+	# main_database = get_video_details(youtube, flatten(all_video_list)[:10])
 
-	print("Translating")
-	all_translated_descriptions = translate(all_descriptions)
+	# all_descriptions = main_database['descriptions']
 
-	for i in range(10):
-		print(all_descriptions[i])
-		print(all_translated_descriptions[i])
+	# print("Translating")
+	# all_translated_descriptions = translate(all_descriptions, 10)
 
-		print('=' * 25)
+	# # for i in range(10):
+	# # 	print(all_descriptions[i])
+	# # 	print(all_translated_descriptions[i])
 
+	# # 	print('=' * 25)
+	# main_database['translated'] = all_translated_descriptions
+
+	# pd.DataFrame.from_dict(main_database).to_csv("main_database_11th_Dec.csv")
+
+
+	## PHASE 2
+	main_database = pd.read_csv("main_database_11th_Dec.csv", index_col=[0])
+	for row_id, values in main_database.iterrows():
+		document = values['translated']
+		keywords_in_video = ask_llm_to_extract_keywords(document)
+		# row_id == video_id
+		# values['publish_date']
+		# 	-> push to SQL
+		print(keywords_in_video)
+		print("=" * 25)
 
 if __name__ == '__main__':
 	main()
